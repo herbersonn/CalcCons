74ed705ebfcfeb4127720b348e14fb005d0640a8
herbersonn <150865147+herbersonn@users.noreply.github.com>
2024-09-07 17:36:50 -0300

    Update Calculo.java

--- Calculo.java
+++ Calculo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Calculo {
 
     // Função para calcular o consumo total de dados móveis
     public static int calcularConsumoTotal(String[] consumoSemanal) {
